4309df2f037a39653b112ca1ae7085b317cccf72
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2022-05-20 20:52:51 +0700

    Create patient-login.blade.php

---
+++ resources/views/role/patient-login.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.patient-login-layout')
+
+@section('mainTitle', 'Staysane - Patient')
+@section('title', 'Welcome to Staysane!')
+@section('signIn', 'Sign In')
+@section('signUp', 'Sign Up')
+@section('username', 'Username')
+@section('password', 'Password')
+@section('name', 'Name')
+@section('email', 'Email')
+@section('DoB', 'Date of Birth')
+@section('address', 'Address')
+@section('back', 'Back')
+
+
+
+
+
+
+
