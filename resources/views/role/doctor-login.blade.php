5ccbeb7fe620c24e843cdddddc203074a54806a5
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2022-05-20 20:52:44 +0700

    Create doctor-login.blade.php

---
+++ resources/views/role/doctor-login.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.doctor-login-layout')
+
+@section('mainTitle', 'Staysane - Doctor')
+@section('title', 'Welcome to Staysane!')
+@section('signIn', 'Sign In')
+@section('signUp', 'Sign Up')
+@section('SIPP', 'SIPP')
+@section('password', 'Password')
+@section('name', 'Name')
+@section('email', 'Email')
+@section('back', 'Back')
+
+
+
+
+
+
+
+
