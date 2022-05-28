c821b5a00998abbcef24eb58b289877f4e3aaf38
noverita <81152377+noverita@users.noreply.github.com>
2022-05-28 20:51:47 +0700

    28/05/2022

--- resources/views/layout/all-layout.blade.php
+++ resources/views/layout/all-layout.blade.php
@@ -1,114 +1,111 @@
-<!DOCTYPE html>
 <html>
 
 <head>
-
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link
-        href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css"
-        rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Francois+One&display=swap" rel="stylesheet">
-
-    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
-    <script
-        src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js">
-    </script>
-
-<style>
-    img {
-            border-radius: 100%;
-            border-style: solid;
-            border-color: rgb(100, 19, 46);
-            box-shadow: 8px 2px #000000;
-        }
-    .sidenav {
-  height: 100%;
-  width: 195px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background: #D0E1D4;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 20px;
-  font-family: 'Francois One', sans-serif;
-  color: white;
-  display: block;
-}
-
-.sidenav a:hover {
-    background: #536D6C;
-        padding: 20px;
-        box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.25);
-    border-radius: 65px;
-}
-
-.main {
-  margin-left: 160px; /* Same as the width of the sidenav */
-  font-size: 28px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-  background: linear-gradient(112.4deg, #FFEEEE 9.84%, #E7DFFF 73.46%);
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-h3 {
-    text-align: center;
-    font-family: 'Francois One', sans-serif;
-}
-.content{
-    text-align: center;
-    font-family: 'Francois One', sans-serif;
-}
-table{
-    margin-left: 2cm;
-  margin-right: 2cm;
-  font-family: 'Francois One', sans-serif;
-  text-align: center
-
-}
-p{
-    text-align: center
-}
-body {
-        background: linear-gradient(112.4deg, #FFEEEE 9.84%, #E7DFFF 73.46%);
-    }
-</style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Francois+One&display=swap" rel="stylesheet">
 </head>
 
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        font-family: 'Fjalla One', sans-serif;
+        font-family: 'Francois One', sans-serif;
+    }
+
+    body {
+        background: linear-gradient(112.4deg, #FFEEEE 9.84%, #E7DFFF 73.46%);
+    }
+
+    .wrapper {
+        position: relative;
+    }
+
+    .sidebar {
+        position: fixed;
+        width: 350px;
+        height: 100%;
+        background: #D0E1D4;
+        padding: 20px 0;
+    }
+
+    .textmuted {
+        color: rgb(255, 255, 255);
+        font-family: 'Fjalla One', sans-serif;
+        font-family: 'Francois One', sans-serif;
+        font-weight: 700;
+        font-size: 23px;
+        position: absolute;
+        width: 112px;
+        height: 35px;
+        left: 140px;
+        top: 41px;
+
+    }
+
+    ul {
+        padding-bottom: 20px;
+        position: absolute;
+        width: 327px;
+        height: 228px;
+        left: 11px;
+        top: 204px;
+    }
+    .sidebar ul li {
+        padding: 15px;
+        font-size: 18px
+    }
+
+    .sidebar ul li a {
+        color: rgb(0, 0, 0);
+        display: block;
+    }
+
+    .sidebar ul li:hover {
+        background: #536D6C;
+        padding: 20px;
+        box-shadow: 0px 5px 10px 2px rgba(0, 0, 0, 0.25);
+	border-radius: 65px;
+    }
+
+    .sidebar ul li a:hover {
+        text-decoration: none;
+        color: rgb(255, 255, 255);
+    }
+    .content {
+        margin-left: 25%
+    }
+</style>
+
 <body>
+    <div class="wrapper d-flex">
+        <div class="sidebar">
+            <div class="textmuted">Staysane</div>
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="#doctor">Doctor</a></li>
+                <li><a href="#chat">Chat</a></li>
+                <li><a href="/profile">Profile</a></li>
+            </ul>
+        </div>
+    </div>
     <div class="container">
-    <div class="sidenav">
-        <a href="/home">Home</a><hr>
-        <a href="/doctor">Doctor</a><hr>
-        <a href="/chat">Chat</a><hr>
-        <a href="/profile">Profile</a>
-      </div>
-
-
-        <h3><b> @yield('judulhalaman')</b></h3>
-
     <div class="content">
-          <table>
-    @section('konten')
+  @section('konten')
 
-    @show
-</table>
+  @show
 </div>
-
+</div>
 </body>
-</div>
+
 </html>
