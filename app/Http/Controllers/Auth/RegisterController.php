d0446bbd925f12f149fcb155adfbb77826c04222
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2022-06-05 22:21:31 +0700

    change column type migration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255'],
-            'DoB' => ['required', 'string', 'max:255'],
+            'DoB' => ['required', 'date'],
             'role' => ['required', 'string', 'max:255'],
             'city' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
