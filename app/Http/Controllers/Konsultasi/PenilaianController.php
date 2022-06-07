d89a52e0e52af3d56e0108b061e12b578e9fd303
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2022-06-07 23:24:45 +0700

    update penilaian

--- app/Http/Controllers/Konsultasi/PenilaianController.php
+++ app/Http/Controllers/Konsultasi/PenilaianController.php
@@ -1,14 +1,28 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Konsultasi;
+
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class PenilaianController extends Controller
 {
     //
     public function penilaianPasien(){
-    	return view('patient.penilaian');
+        $listakunpsikolog = DB::table('listakunpsikolog')->get();
+        return view('patient.penilaian',['listakunpsikolog' => $listakunpsikolog]);
+       
     }
 
+    public function storePenilaian(Request $request)
+    {
+        DB::table('penilaian')->insert([
+		'sipp' => $request->sipp,
+        'rating' => $request->rating,
+		'reviewtext' => $request->reviewtext
+	]);
+    return redirect('/list/psikolog');
+}
 }
--- resources/views/patient/penilaian.blade.php
+++ resources/views/patient/penilaian.blade.php
@@ -14,7 +14,8 @@
     </nav><!-- .nav-menu -->
 
 @endsection
-<link rel='stylesheet' id='fontawesome-css' href='https://use.fontawesome.com/releases/v5.0.1/css/all.css?ver=4.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='fontawesome-css' href='https://use.fontawesome.com/releases/v5.0.1/css/all.css?ver=4.9.1'
+    type='text/css' media='all' />
 <style>
     .container p-3 my-3 {
         position: absolute;
@@ -55,6 +56,7 @@
         line-height: 27px;
         text-align: center;
         letter-spacing: -0.165px;
+        /* white-space: nowrap; */
     }
 
     .catatan-bttn {
@@ -74,6 +76,10 @@
         border: #536D6C;
     }
 
+    .profile {
+        align-content: center;
+    }
+
     .w3-container {
         height: 232px;
         width: 202px;
@@ -82,131 +88,179 @@
         align-content: center;
     }
 
+    .img-container {
+        align-content: center;
+    }
+
     #piccatatan {
         align: center;
     }
-    fieldset, label { margin: 0; padding: 0; }
-body{ margin: 20px; }
-h1 { font-size: 1.5em; margin: 10px; }
 
-/****** Style Star Rating Widget *****/
+    fieldset,
+    label {
+        margin: 0;
+        padding: 0;
+    }
 
-.rating {
-      border: none;
-      margin-right: 49px;
-}
-.myratings{
+    body {
+        margin: 20px;
+    }
 
-  font-size: 85px;
-  color: green;
-}
+    h1 {
+        font-size: 1.5em;
+        margin: 10px;
+    }
 
-.rating > [id^="star"] { display: none; }
-.rating > label:before {
-  margin: 5px;
-  font-size: 2.25em;
-  font-weight
-  display: inline-block;
-  content: "\f005";
-  font-family: "Font Awesome 5 Free Solid", "Font Awesome 5 Free" !important;
-font-weight: 400;
-}
+    /****** Style Star Rating Widget *****/
 
+    .rating {
+        border: none;
+        margin-right: 49px;
+    }
 
-.rating > label {
-  color: rgb(128, 116, 116);
- float: right;
-}
+    .myratings {
 
-/***** CSS Magic to Highlight Stars on Hover *****/
+        font-size: 85px;
+        color: green;
+    }
 
-.rating > [id^="star"]:checked ~ label, /* show gold star when clicked */
-.rating:not(:checked) > label:hover, /* hover current star */
-.rating:not(:checked) > label:hover ~ label { color: #ffd900ee;  } /* hover previous stars in list */
-.rating > [id^="star"]:checked + label:hover, /* hover current star when changing rating */
-.rating > [id^="star"]:checked ~ label:hover,
-.rating > label:hover ~ [id^="star"]:checked ~ label, /* lighten current selection */
-.rating > [id^="star"]:checked ~ label:hover ~ label { color: #FFED85;  }
+    .rating>[id^="star"] {
+        display: none;
+    }
 
-.reset-option {
-display: none;
-}
-
-.reset-button {
-margin: 6px 12px;
-background-color: rgb(255, 255, 255);
-text-transform: uppercase;
-}
+    .rating>label:before {
+        margin: 5px;
+        font-size: 2.25em;
+        font-weight display: inline-block;
+        content: "\f005";
+        font-family: "Font Awesome 5 Free Solid", "Font Awesome 5 Free" !important;
+        font-weight: 400;
+    }
 
 
+    .rating>label {
+        color: rgb(128, 116, 116);
+        float: right;
+    }
+
+    /***** CSS Magic to Highlight Stars on Hover *****/
+
+    .rating>[id^="star"]:checked~label,
+    /* show gold star when clicked */
+    .rating:not(:checked)>label:hover,
+    /* hover current star */
+    .rating:not(:checked)>label:hover~label {
+        color: #ffd900ee;
+    }
+
+    /* hover previous stars in list */
+    .rating>[id^="star"]:checked+label:hover,
+    /* hover current star when changing rating */
+    .rating>[id^="star"]:checked~label:hover,
+    .rating>label:hover~[id^="star"]:checked~label,
+    /* lighten current selection */
+    .rating>[id^="star"]:checked~label:hover~label {
+        color: #FFED85;
+    }
+
+    .reset-option {
+        display: none;
+    }
+
+    .reset-button {
+        margin: 6px 12px;
+        background-color: rgb(255, 255, 255);
+        text-transform: uppercase;
+    }
 </style>
 @section('content')
 
 
     <div class="penilaian">
         <div class="container">
+
             <div class="row profile">
                 <center>
                     <div class="col-md-3">
-                        <div class="pic">
-                            <img alt="docpic" src="/images/doctorprofile3.png">
+                        <div class="img-container">
+                            <img alt="docpic" style="vertical-align:middle" src="/images/doctorprofile3.png">
                         </div>
                         <br>
-                        <div class="col patientname">
-                            <p>Alex Nikiforov</p>
+
+                        <div class="col docname">
+                            <p style="vertical-align:middle">Dr. Nike Ardilla, M.Psi.</p>
                         </div>
                 </center>
+
             </div>
+
         </div>
         <div class="container mt-3 mb-4">
-            <form action="/patient/profile/update" method="POST" enctype="multipart/form-data">
+            <form action="/submitpenilaian" method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
-                @foreach($penilaian as $p)
-                <input type="hidden" name="id" value="{{ $p->idpasien }}"> <br>
-                <div class="form-group">
-                    <label for="pass">Username:</label>
-                    <input type="text" class="form-control"  name="uname" value="{{ $p->unamepasien }}">
-                  </div>
-                <div class="form-group">
-                    <label for="nama">Nama:</label>
-                    <input type="text" class="form-control"  name="nama" value="{{ $p->namapasien}}">
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control"  name="email" value="{{ $p->emailpasien }}">
-                  </div>
-            <form action="/patient/chats/penilaian" method="POST" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                @foreach($penilaian as $s)
-                <input type="hidden" name="idpasien" value="{{ $s->idpasien }}"> <br>
-                <input type="hidden" name="idpsikolog" value="{{ $s->sipp }}"> <br>
+                {{-- value="{{ $s->sipp }} --}}
+                <div class="form-group row">
+                    <label for="nama" class="col-sm-3 control-label">Nama Psikolog:</label>
+                    <div class='col-sm-8 input-group date' id='sipp'>
+                        <select class="form-control" name="sipp">
+                            @foreach ($listakunpsikolog as $p)
+                                    <option value="{{ $p->sipp }}"> {{ $p->namapsikolog }}</option>
+                                @endforeach
+                        </select>
+                    </div>
+                </div>
+                <br>
                 <div class="form-group row">
                     <label for="rating">Rating:</label>
-                <fieldset class="rating">
-                    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Outstanding - 5 stars"></label>
+                    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+                    {{-- <form>
+  <input type="radio" class="star-input" name="rating" id="star-1" value="1">
+  <label for="star-1" class="star"><i class="fas fa-star"></i></label>
+  <input type="radio" class="star-input" name="rating" id="star-2" value="2">
+  <label for="star-2" class="star"><i class="fas fa-star"></i></label>
+  <input type="radio" class="star-input" name="rating" id="star-3" value="3">
+  <label for="star-3" class="star"><i class="fas fa-star"></i></label>
+  <input type="radio" class="star-input" name="rating" id="star-4" value="4">
+  <label for="star-4" class="star"><i class="fas fa-star"></i></label>
+  <input type="radio" class="star-input" name="rating" id="star-5" value="5" checked>
+  <label for="star-5" class="star"><i class="fas fa-star"></i></label>
+  <button type="submit">Send</button>
+</form> --}}
+<div class='col-sm-8' id='rating'>
+                    <fieldset class="rating">
+                        <input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5"
+                            title="Outstanding - 5 stars" if></label>
 
-                    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Very Satisfied - 4 stars"></label>
+                        <input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4"
+                            title="Very Satisfied - 4 stars"></label>
 
-                    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Satisfied - 3 stars"></label>
+                        <input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3"
+                            title="Satisfied - 3 stars"></label>
 
-                    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Unsatisfied - 2 stars"></label>
+                        <input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2"
+                            title="Unsatisfied - 2 stars"></label>
 
-                    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Poor - 1 star"></label>
+                        <input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1"
+                            title="Poor - 1 star"></label>
 
-                    <input type="radio" class="reset-option" name="rating" value="reset" />
-                </fieldset>
+                        <input type="radio" class="reset-option" name="rating" value="reset" />
+                    </fieldset>
                 </div>
+            </div>
 
                 <div class="form-group row">
-                    <label for="comment">Penilaian:</label>
-                    <textarea class="form-control" rows="5" id="comment">
-                   </textarea>
+                    <label for="comment">Review:</label>
+                    <div class='col-sm-14' id='reviewtext'>
+                    <textarea class="form-control" for="reviewtext" name="reviewtext" rows="5" id="comment" placeholder="How was your session?"></textarea>
                     <br>
-                    <input type="submit" class="btn catatan-bttn" value="OK">
-                </div>
+                    <input type="submit" class="btn catatan-bttn" value="Submit">
+                    {{-- <input type="submit" class="btn catatan-bttn" value="OK"><a href="/patient/doctor" style="color: white"> Submit </a> --}}
 
+            </div>
+            </div>
+                {{-- @endforeach --}}
             </form>
-            <script>
+            {{-- <script>
             $(document).ready(function(){
 
                 $("input[type='radio']").click(function(){
@@ -223,7 +277,7 @@ text-transform: uppercase;
 
 
                 });
-</script>
+</script> --}}
         </div>
     </div>
 
