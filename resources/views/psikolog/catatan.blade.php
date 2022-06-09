b8d791a73e607b11265655ea7d3f0222c405ea5c
ndtsaniyah <tsaniyah.coshack1019@gmail.com>
2022-06-09 09:20:44 +0700

    Y

--- resources/views/psikolog/catatan.blade.php
+++ resources/views/psikolog/catatan.blade.php
@@ -200,7 +200,7 @@
                             <img alt="docpic" style="vertical-align:middle" src="/images/doctorprofile3.png">
                         </div>
                         <br>
-                    </div>
+                    </div>89
                 </center>
 
             </div>
@@ -209,6 +209,7 @@
         <div class="container mt-3 mb-4">
             <form action="/submitcatatan" onsubmit="return (validate());" method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
+                {{-- value="{{ $s->sipp }} --}}
                 <div class="form-group row">
                     <label for="nama" class="col-sm-3 control-label">Nama Pasien:</label>
                     <div class='col-sm-8 input-group date' id='id'>
@@ -227,9 +228,10 @@
                     <br>
                     <br>
                     <input type="submit" class="btn catatan-bttn" value="Submit">
-
+                    {{-- <input type="submit" class="btn catatan-bttn" value="OK"><a href="/patient/doctor" style="color: white"> Submit </a> --}}
                 </div>
             </div>
+                {{-- @endforeach --}}
             </form>
 
 
